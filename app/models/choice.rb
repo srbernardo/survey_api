b877cb94e50c77bd8571c309e0db8204f62a31d9
bernardo brotelli <b.brotelle@gmail.com>
2024-09-04 19:15:24 -0300

    Add "limit_choices_for_radio_button_questions" method to Choice model

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,3 +1,13 @@
 class Choice < ApplicationRecord
   belongs_to :question
+
+  validate :limit_choices_for_radio_button_questions, on: :create
+
+  private
+
+  def limit_choices_for_radio_button_questions
+    if question.option == 'radio_button' && question.choices.count >= 5
+      errors.add(:base, 'You cannot add more than 5 "choices" for this question.')
+    end
+  end
 end
