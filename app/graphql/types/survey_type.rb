e48f4ffe68a67dc47bf76eb4f403f9769e5b6650
bernardo brotelli <b.brotelle@gmail.com>
2024-09-05 10:57:20 -0300

    Add result field to survey_type

--- app/graphql/types/survey_type.rb
+++ app/graphql/types/survey_type.rb
@@ -6,7 +6,12 @@ module Types
     field :title, String, null: false
     field :user_id, Integer, null: false
     field :questions, [QuestionType], null: false, description: "List of questions for this Survey"
+    field :result, GraphQL::Types::JSON, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def result
+      object.result
+    end
   end
 end
