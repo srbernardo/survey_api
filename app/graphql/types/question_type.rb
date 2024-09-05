f1488849c4f1b10182349b0fe128b82d0f655959
bernardo brotelli <b.brotelle@gmail.com>
2024-09-05 10:56:52 -0300

    Add choice_answers field to question_type and adjust option to string

--- app/graphql/types/question_type.rb
+++ app/graphql/types/question_type.rb
@@ -4,9 +4,10 @@ module Types
   class QuestionType < Types::BaseObject
     field :id, ID, null: false
     field :title, String, null: false
-    field :option, Integer, null: false
+    field :option, String, null: false
     field :survey_id, Integer, null: false
     field :choices, [Types::ChoiceType], null: false, description: "List of choices for this question"
+    field :choice_answers, [Types::ChoiceAnswerType], null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
