2c1a2f22313d0400e87716642fb16cf375ccff56
bernardo brotelli <b.brotelle@gmail.com>
2024-09-05 10:55:59 -0300

    Add choice_answers field to choice_type

--- app/graphql/types/choice_type.rb
+++ app/graphql/types/choice_type.rb
@@ -4,9 +4,13 @@ module Types
   class ChoiceType < Types::BaseObject
     field :id, ID, null: false
     field :value, String
-    field :marked, Boolean
     field :question_id, Integer, null: false
+    field :choice_answers, [Types::ChoiceAnswerType], null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def choice_answers
+      object.choice_answers
+    end
   end
 end
