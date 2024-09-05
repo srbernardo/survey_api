c771764a19e1864c97e8a7e1d5ad6aad358e7c44
bernardo brotelli <b.brotelle@gmail.com>
2024-09-04 23:43:28 -0300

    Add all mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,15 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :choice_respondent_update, mutation: Mutations::ChoiceRespondentUpdate
+    field :multi_line_answer_create, mutation: Mutations::MultiLineAnswerCreate
+    field :single_line_answer_create, mutation: Mutations::SingleLineAnswerCreate
+    field :choice_update, mutation: Mutations::ChoiceUpdate
+    field :question_update, mutation: Mutations::QuestionUpdate
+    field :question_delete, mutation: Mutations::QuestionDelete
+    field :choice_delete, mutation: Mutations::ChoiceDelete
+    field :choice_create, mutation: Mutations::ChoiceCreate
+    field :question_create, mutation: Mutations::QuestionCreate
     field :survey_delete, mutation: Mutations::SurveyDelete
     field :survey_update, mutation: Mutations::SurveyUpdate
     field :survey_create, mutation: Mutations::SurveyCreate
