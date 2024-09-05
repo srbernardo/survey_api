e581afd42c3d4fa95ba841d45a1da2d26bfe7603
bernardo brotelli <b.brotelle@gmail.com>
2024-09-04 23:43:13 -0300

    Add questions field to survey_type

--- app/graphql/types/survey_type.rb
+++ app/graphql/types/survey_type.rb
@@ -5,6 +5,7 @@ module Types
     field :id, ID, null: false
     field :title, String, null: false
     field :user_id, Integer, null: false
+    field :questions, [QuestionType], null: false, description: "List of questions for this Survey"
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
