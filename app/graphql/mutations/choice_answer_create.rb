c949a43e18d4b83ff55aa97bed62b976c6ed8046
bernardo brotelli <b.brotelle@gmail.com>
2024-09-06 08:33:31 -0300

    Adjust on raise error of choice_answer_create mutation

--- app/graphql/mutations/choice_answer_create.rb
+++ app/graphql/mutations/choice_answer_create.rb
@@ -11,8 +11,8 @@ module Mutations
 
     def resolve(choice_id:, user_id:)
       choice_answer = ::ChoiceAnswer.new(choice_id:, user_id:)
-      raise GraphQL::ExecutionError.new "Error: Survey closed", extensions: choice_answer.errors.to_hash unless choice_answer.question.survey.open?
       raise GraphQL::ExecutionError.new "Error creating choice_answer", extensions: choice_answer.errors.to_hash unless choice_answer.save
+      raise GraphQL::ExecutionError.new "Error: Survey closed", extensions: choice_answer.errors.to_hash unless choice_answer.question.survey.open?
 
       { choice_answer: choice_answer }
     end
