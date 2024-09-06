5a70b49c02e24bd88dcfdf453ff47016a3f508d4
bernardo brotelli <b.brotelle@gmail.com>
2024-09-06 10:22:11 -0300

    Remove completed_surveys from surveys_lists type

--- app/graphql/types/surveys_lists_type.rb
+++ app/graphql/types/surveys_lists_type.rb
@@ -1,7 +1,6 @@
 module Types
   class SurveysListsType < Types::BaseObject
-    field :completed_surveys, [Types::SurveyType], null: false, description: "List of completed surveys"
-    field :open_surveys, [Types::SurveyType], null: false, description: "List of incomplete surveys"
-    field :close_surveys, [Types::SurveyType], null: false, description: "List of incomplete surveys"
+    field :open_surveys, [Types::SurveyType], null: false, description: "List of open surveys"
+    field :close_surveys, [Types::SurveyType], null: false, description: "List of close surveys"
   end
 end
